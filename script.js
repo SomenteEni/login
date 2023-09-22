93446506b9cf5204b22e201369f32c1494190350
Enivaldo Oliveira <145685688+SomenteEni@users.noreply.github.com>
2023-09-21 22:12:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let buttonElement = document.querySelector("button");	
+
+let correctPassword =  document.querySelector("correctPass");
+
+let justPass = "justatest1234";
+
+var alert = "Senha correta!";
+
+function checkPassword(correctPassword, justPass){
+
+	if (correctPassword == justPass) {
+		alert("Teste");
+	}
+
+}
+
